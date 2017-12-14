e62530389ec37c2160f7d83cd74d242f57005f1a
Ginjinator <32849705+Ginjinator@users.noreply.github.com>
2017-12-13 20:26:00 -0500

    Delete BussinessNameGenerator.java

--- src/Week15Final/NameGenerator/BussinessNameGenerator.java
+++
@@ -1,9 +0,0 @@
- /*
- This class will read a bussiness name from the list
- and turn it into a readable and usable string
- */
-package Week15Final.NameGenerator;
-
-public class BussinessNameGenerator {
-
-}
